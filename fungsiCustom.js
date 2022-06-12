28b0842457045cf0e12663ec253b34cb5965ff38
restuhanputra <restu.hanputraa@gmail.com>
2022-06-12 21:35:39 +0700

    update: challenge06

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,6 @@
 // TODO: import module bila dibutuhkan di sini
+/* fs = FileReader API node js*/
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +20,50 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallBack) => {
+  let resultArray = []
+
+  /* reaad file json
+    fs.readFIle('nama file', 'option/string encoding', (fnCallback))
+
+    dengan set encoding
+  */
+  fs.readFile(
+    file1, 
+    {
+      encoding : "utf8"
+    },
+    (err, data)=> {
+      if (err) {
+        return fnCallBack(err, null);
+      }
+      
+      const dataResult = JSON.parse(data);
+      resultArray.push(dataResult.message.split(" ")[1]); //split data aray ke 1
+    }
+  )
+
+  /* langsung utf8 */
+  fs.readFile(file2, 'utf8', (err, data)=> {
+    if (err) {
+      return fnCallBack(err, null);
+    }
+
+      const dataResult = JSON.parse(data);
+      resultArray.push(dataResult[0].message.split(" ")[1]);
+  });
+
+
+  fs.readFile(file3, 'utf8', (err, data) => {
+    if (err) {
+      return fnCallBack(err, null);
+    }
+
+    const dataResult = JSON.parse(data);
+    resultArray.push(dataResult[0].data.message.split(" ")[1]);
+    return fnCallBack(null, resultArray);
+  });
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
